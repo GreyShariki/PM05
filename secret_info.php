c2edc959a7c5fb0b8b66a24001cf75857aa913a9
GreyShariki <154407622+GreyShariki@users.noreply.github.com>
2024-07-02 19:28:54 +0700

    Создание личного кабинета

---
+++ secret_info.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Личный кабинет</title>
+    <link rel="stylesheet" href="style1.css">
+    
+</head>
+<body>
+    <div class="container">
+        <div class="profile-info">
+            <img src="image.png" alt="Аватар" class="avatar">
+            <div class="user-details">
+                <h2>Алексей Кузнецов</h2>
+                <p>Телефон: +7 999 123 85 55</p>
+                <p>Почта: alec.kyz@example.com</p>
+            </div>
+        </div>
+
+        <div class="content">
+            <h3>Мой влог</h3>
+            <p>Я — человек, который ценит баланс в жизни. Работу воспринимаю как возможность развиваться и расти, но не забываю о важности личного времени и увлечений. </p>
+            <p>Фотография - это моя страсть, способ запечатлеть моменты и делиться ими с близкими. Люблю ловить свет, экспериментировать с композицией и передавать эмоции через объектив. </p>
+            <div class="container1">
+        <div class="photo">
+            <img src="photo1.jpg" alt="Фото 1">
+        </div>
+        <div class="photo">
+            <img src="photo2.jpg" alt="Фото 2">
+        </div>
+        <div class="photo">
+            <img src="photo3.jpg" alt="Фото 3">
+        </div>
+        <div class="photo">
+            <img src="photo4.jpg" alt="Фото 4">
+        </div>
+      
+    </div>
+        </div>
+    </div>
+</body>
+</html>
